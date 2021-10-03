7fd2c40697e959261ab91d254482f87696e0aedb
benjamin <308686465@qq.com>
2021-10-03 10:34:54 +0800

    添加左侧菜单错误码

--- pkg/e/msg.go
+++ pkg/e/msg.go
@@ -5,15 +5,6 @@ var MsgFlags = map[int]string{
 	ERROR:          "fail",
 	INVALID_PARAMS: "请求参数错误",
 
-	ERROR_NOT_EXIST_PRODUCT:        "该商品不存在",
-	ERROR_CHECK_EXIST_PRODUCT_FAIL: "检查商品是否存在失败",
-	ERROR_ADD_PRODUCT_FAIL:         "新增商品失败",
-	ERROR_UPDATA_PRODUCT_FAIL:      "更新商品失败",
-	ERROR_GET_PRODUCTS_FAIL:        "获取多个商品失败",
-	ERROR_GET_PRODUCTS_NUMBER_FAIL: "获取商品数目失败",
-	ERROR_GET_PRODUCT_FAIL:         "获取单个商品失败",
-	ERROR_OUT_OF_STOCK:             "库存不足",
-
 	ERROR_NOT_EXIST_ORDER:            "该订单不存在",
 	ERROR_CHECK_EXIST_ORDER_FAIL:     "检查订单是否存在失败",
 	ERROR_ADD_ORDER_FAIL:             "新增订单失败",
@@ -32,6 +23,8 @@ var MsgFlags = map[int]string{
 
 	ERROR_AUTH_CHECK_TOKEN_TIMEOUT: "token已超时",
 	ERROR_GENERATE_TOKEN_FAIL:      "token生成失败",
+
+	ERROR_GET_MENUS_ERROR: "获取左侧菜单失败",
 }
 
 func GetMsg(code int) string {
